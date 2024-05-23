99e50f812c00e07e1b8363d99d082e3c910caa3b
Zahra Honari <z.honari@gmail.com>
2024-05-23 11:51:33 +0330

    fibo function added

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-for i in range (10) :
-    i = i+1 
+import fibo
+
+def total(n):
+    t = 0
+    for i in range (10) :
+        t += i
+    
+    return t
+
+if __name__ == "__main__":
+    print(total(10))
+    print(fibo(12))
